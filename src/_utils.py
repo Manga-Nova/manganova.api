4fb39a122c457fa559391ad39ee5cd9c1c50c5c4
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-25 17:23:14 -0300

    refactor: remove zoneinfo import from current_datetime function

--- src/_utils.py
+++ src/_utils.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from zoneinfo import ZoneInfo
 
 
 def current_datetime() -> datetime:
-    return datetime.now(ZoneInfo("UTC"))
+    return datetime.now()  # noqa: DTZ005
