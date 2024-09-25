640134f69cea872f2f9cac40c4f9c394eb442db7
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-25 17:59:23 -0300

    feat: Add UserNotFoundError exception and update get_user_by_id endpoint
    - Add UserNotFoundError exception to handle cases when a user is not found
    - Update get_user_by_id endpoint to use the new exception and return a response model
    - Change the user_id parameter type to int in the get_user_by_id endpoint
    - Update the query statement in the get_user_by_id endpoint

--- src/modules/user/controller.py
+++ src/modules/user/controller.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import Body, Path
 
 from src.core.router import ApiRouter
+from src.exceptions.not_found import UserNotFoundError
 from src.modules.user.dtos import CreateUserDTO, ExportUserDTO
 from src.modules.user.repository import UserRepository
 from src.modules.user.service import UserService
@@ -12,13 +13,19 @@ router = ApiRouter(prefix="/user", tags=["user"])
 SERVICE = UserService(UserRepository())
 
 
-@router.get(path="/{userId}")
-async def get_user_by_id(userId: Annotated[str, Path()]) -> ExportUserDTO:
+@router.get(
+    path="/{user_id}",
+    response_model=ExportUserDTO,
+    exceptions=[UserNotFoundError(userId=1234)],
+)
+async def get_user_by_id(
+    user_id: Annotated[int, Path()],
+) -> ExportUserDTO:
     """Get a user by ID."""
-    return await SERVICE.get_user_by_id(userId)
+    return await SERVICE.get_user_by_id(user_id)
 
 
-@router.post(path="")
+@router.post(path="", response_model=ExportUserDTO)
 async def create_user(user: Annotated[CreateUserDTO, Body()]) -> ExportUserDTO:
     """Create a user."""
     return await SERVICE.create_user(user)
