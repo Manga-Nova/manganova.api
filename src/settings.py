540a0076d90f9e07c998cf7ea966582d61edf520
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-25 16:34:44 -0300

    refactor: update database connection settings in settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from src._version import get_version
@@ -6,18 +8,20 @@ from src._version import get_version
 class _Settings(BaseSettings):
     model_config = SettingsConfigDict(case_sensitive=False, env_file=None)
 
+    ENV: Literal["prod", "dev", "test"] = "dev"
+
     APP_NAME: str = "Manga Nova API"
     APP_VERSION: str = get_version()
 
-    POSTGRE_USER: str = "postgres"
-    POSTGRE_PASSWORD: str = "postgres"
-    POSTGRE_HOST: str = "localhost"
-    POSTGRE_PORT: int = 5432
-    POSTGRE_DB: str = "postgres"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = "postgres"
 
     @property
-    def postgre_url(self) -> str:
-        return f"postgresql://{self.POSTGRE_USER}:{self.POSTGRE_PASSWORD}@{self.POSTGRE_HOST}:{self.POSTGRE_PORT}/{self.POSTGRE_DB}"
+    def postgres_url(self) -> str:
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
 
 Settings = _Settings()
