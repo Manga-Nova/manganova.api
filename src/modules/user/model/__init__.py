f2f2068a9d5a6ebc5a4eab632ef6c108bb1d3e2f
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-25 17:22:54 -0300

    refactor: update User model table name and column defaults
    - Update the table name in the User model from "db_users2" to "db_users".
    - Change the default values for the "created_at" and "updated_at" columns to use the current datetime.
    - Add unique constraints and indexes to the "username" and "email" columns.

--- src/modules/user/model/__init__.py
+++ src/modules/user/model/__init__.py
@@ -1,24 +1,22 @@
-import uuid
 from datetime import datetime
-from zoneinfo import ZoneInfo
 
 from sqlalchemy.orm import Mapped, mapped_column
 
+from src._utils import current_datetime
 from src.modules.base.model import ModelBase
 
 
 class User(ModelBase):
     """User model."""
 
-    __tablename__ = "db_users2"
+    __tablename__ = "db_users"
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now(ZoneInfo("UTC")))
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(default=current_datetime())
     updated_at: Mapped[datetime] = mapped_column(
-        default=datetime.now(ZoneInfo("UTC")),
-        onupdate=datetime.now(ZoneInfo("UTC")),
-        nullable=False,
+        default=current_datetime(),
+        onupdate=current_datetime(),
     )
-    username: Mapped[str]
-    email: Mapped[str]
+    username: Mapped[str] = mapped_column(unique=True, index=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
     password: Mapped[str]
