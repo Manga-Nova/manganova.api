b71bf7b25b477222987f7e122a340e68956e6126
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-25 19:41:43 -0300

    feat: add crypt helper class to aggregate encode and decode functions

---
+++ src/core/crypt.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+from typing import Any
+
+from bcrypt import checkpw, gensalt, hashpw
+from jwt import decode, encode
+
+from src._utils import current_datetime
+from src.settings import Settings
+
+
+class CryptHelper:
+    """Crypt helper."""
+
+    @classmethod
+    def encode(cls, payload: dict[str, Any], *, stay_logged_in: bool = False) -> str:
+        """Encode payload."""
+        if stay_logged_in:
+            return str(encode(payload, key=Settings.JWT_SECRET, algorithm="HS256"))
+
+        payload.update({"exp": current_datetime() + timedelta(days=1)})
+        return str(encode(payload, key=Settings.JWT_SECRET, algorithm="HS256"))
+
+    @classmethod
+    def decode(cls, token: str) -> dict[str, Any]:
+        """Decode token."""
+        return dict(decode(token, key=Settings.JWT_SECRET, algorithms=["HS256"]))
+
+    @classmethod
+    def hash_password(cls, password: str) -> str:
+        """Hash password."""
+        return str(hashpw(password.encode(), gensalt()))
+
+    @classmethod
+    def check_password(cls, password: str, hashed_password: str) -> bool:
+        """Check password."""
+        return checkpw(
+            password=password.encode(),
+            hashed_password=hashed_password.encode(),
+        )
