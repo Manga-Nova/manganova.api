89c2fb2b4f11f73fdb4d64f9f0d7cd19646ba747
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-25 17:50:51 -0300

    feat: Add handler method to ApiError class
    The commit adds a new static method `handler` to the `ApiError` class in the `exceptions/base.py` file. This method is responsible for handling API errors and returning a JSON response with the appropriate status code and content. The method takes a `Request` object and an `ApiError` object as parameters.
    
    The purpose of this change is to improve the error handling functionality in the application.

--- src/exceptions/base.py
+++ src/exceptions/base.py
@@ -1,4 +1,9 @@
-from typing import Any
+from typing import TYPE_CHECKING, Any
+
+from fastapi.responses import JSONResponse
+
+if TYPE_CHECKING:
+    from fastapi import Request
 
 
 class ApiError(Exception):
@@ -15,3 +20,11 @@ class ApiError(Exception):
         self.statusCode = status_code
         self.metadata = {**metadata}
         super().__init__(message)
+
+    @staticmethod
+    async def handler(_: "Request", exc: "ApiError") -> JSONResponse:
+        """Handle an API error."""
+        return JSONResponse(
+            status_code=exc.statusCode,
+            content=exc.__dict__,
+        )
