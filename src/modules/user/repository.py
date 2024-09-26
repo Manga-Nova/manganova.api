469d1f8e8d6a19ca91f04fa867c44c8dd0a2778d
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-26 18:31:55 -0300

    refactor: move get_user_by_id, get_user_by_email, and get_user_by_username methods to BaseRepository class

--- src/modules/user/repository.py
+++ src/modules/user/repository.py
@@ -1,37 +1,27 @@
 from sqlalchemy import select
 
-from src.modules.base.db_context import DatabaseContext
+from src.modules.base.repository import BaseRepository
 from src.modules.user.model import User
 
 
-class UserRepository:
+class UserRepository(BaseRepository):
     """User repository."""
-
-    _session = DatabaseContext.session_maker()
 
     async def get_user_by_id(self, user_id: int) -> User | None:
         """Get a user by ID."""
-        async with self._session() as session:
-            return (
-                (await session.execute(select(User).where(User.id == user_id)))
-                .scalars()
-                .first()
-            )
+
+        return (
+            await self._execute_query(select(User).where(User.id == user_id))
+        ).first()
 
     async def get_user_by_email(self, email: str) -> User | None:
         """Get a user by email."""
-        async with self._session() as session:
-            return (
-                (await session.execute(select(User).where(User.email == email)))
-                .scalars()
-                .first()
-            )
+        return (
+            await self._execute_query(select(User).where(User.email == email))
+        ).first()
 
     async def get_user_by_username(self, username: str) -> User | None:
         """Get a user by username."""
-        async with self._session() as session:
-            return (
-                (await session.execute(select(User).where(User.username == username)))
-                .scalars()
-                .first()
-            )
+        return (
+            await self._execute_query(select(User).where(User.username == username))
+        ).first()
