7364fdaa574f569f935639050c214975ae7e16bc
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-26 00:49:23 -0300

    feat: add max lenght to user params username and email

--- src/modules/user/model.py
+++ src/modules/user/model.py
@@ -1,6 +1,8 @@
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.modules.base.model import ModelBase
+from src.settings import Settings
 
 
 class User(ModelBase):
@@ -8,8 +10,14 @@ class User(ModelBase):
 
     __tablename__ = "db_users"
 
-    username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[str] = mapped_column(
+        __type_pos=String(Settings.USERNAME_MAX_LENGHT),
+        unique=True,
+    )
+    email: Mapped[str] = mapped_column(
+        __type_pos=String(Settings.EMAIL_MAX_LENGHT),
+        unique=True,
+    )
     password: Mapped[str]
 
     def __repr__(self) -> str:
