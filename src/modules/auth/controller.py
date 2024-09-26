23efb2c3a05af26c04ae8f36f1164527fdcbf028
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-26 18:01:01 -0300

    feat: add auth routes documentation (response and possible exceptions)

--- src/modules/auth/controller.py
+++ src/modules/auth/controller.py
@@ -1,11 +1,20 @@
 from typing import Annotated
 
-from fastapi import Body, Request
+from fastapi import Body, Request, status
 
 from src.core.crypt import CryptHelper
 from src.core.router import ApiRouter
+from src.exceptions.bad_request import (
+    InvalidEmailError,
+    InvalidPasswordError,
+    InvalidUsernameError,
+    PasswordsDoNotMatchError,
+)
+from src.exceptions.conflict import PasswordAlreadyUsedError, UsernameAlreadyExistsError
+from src.exceptions.unauthorized import EmailOrPasswordError
 from src.modules.auth.dtos import (
     ChangePasswordParams,
+    ChangePasswordResponse,
     LoginParams,
     LoginResponse,
     RegisterParams,
@@ -18,22 +27,47 @@ router = ApiRouter(prefix="/auth", tags=["auth"])
 SERVICE = AuthService(AuthRepository(), CryptHelper())
 
 
-@router.post(path="/login")
+@router.post(
+    path="/login",
+    response_model=LoginResponse,
+    exceptions=[EmailOrPasswordError()],
+)
 async def login(params: Annotated[LoginParams, Body()]) -> LoginResponse:
     """Login with email and password."""
     return await SERVICE.login(params)
 
 
-@router.post(path="/register")
+@router.post(
+    path="/register",
+    response_model=LoginResponse,
+    exceptions=[
+        EmailOrPasswordError(),
+        InvalidUsernameError(),
+        InvalidEmailError(),
+        InvalidPasswordError(),
+        UsernameAlreadyExistsError(),
+    ],
+)
 async def register(params: Annotated[RegisterParams, Body()]) -> LoginResponse:
     """Register a new user."""
     return await SERVICE.register(params)
 
 
-@router.post(path="/change-password", requires_login=True)
+@router.post(
+    path="/change-password",
+    requires_login=True,
+    status_code=status.HTTP_202_ACCEPTED,
+    response_model=ChangePasswordResponse,
+    exceptions=[
+        PasswordAlreadyUsedError(),
+        PasswordsDoNotMatchError(),
+        EmailOrPasswordError(),
+    ],
+)
 async def change_password(
     request: Request,
     params: Annotated[ChangePasswordParams, Body()],
-) -> None:
+) -> ChangePasswordResponse:
     """Change the password of the current user."""
-    return await SERVICE.change_password(request.state.user_id, params)
+    await SERVICE.change_password(request.state.user.user_id, params)
+    return ChangePasswordResponse()
