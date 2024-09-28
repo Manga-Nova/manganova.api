99eac7aca8de908e43e93e4141ab6f9bcaa43865
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-27 23:08:38 -0300

    feat: add title and tag routers to main router

--- src/modules/router.py
+++ src/modules/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from src.modules.auth.controller import router as auth_router
+from src.modules.tag.controller import router as tag_router
+from src.modules.title.controller import router as title_router
 from src.modules.user.controller import router as user_router
 
 router = APIRouter()
@@ -9,3 +11,5 @@ router.include_router(auth_router)
 # Auth should always be first
 
 router.include_router(user_router)
+router.include_router(title_router)
+router.include_router(tag_router)
