74954785750f3e3dab7586deb12eacd462c13688
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-09-29 10:19:51 -0300

    fix: update Payload model to include validation alias for user_id

--- src/modules/auth/dtos.py
+++ src/modules/auth/dtos.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from src.modules.user.dtos import User
 
@@ -42,7 +42,7 @@ class ChangePasswordResponse(BaseModel):
 class Payload(BaseModel):
     """Payload model."""
 
-    user_id: int
+    user_id: int = Field(validation_alias="id")
     email: str
     username: str
     stay_logged_in: bool
