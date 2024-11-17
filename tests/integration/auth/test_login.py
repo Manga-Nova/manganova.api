10dae49e831f24c6ade884da91386e182fa8a6b2
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-11-17 12:03:16 -0300

    refactor: Simplify login tests by using autouse fixture and extracting common logic

--- tests/integration/auth/test_login.py
+++ tests/integration/auth/test_login.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 class TestAuthLoginSuite:
     _PATH = "/auth/login"
 
-    @pytest.fixture(autouse=False)
+    @pytest.fixture(autouse=True)
     def fixture_user(self, client: "TestClient") -> None:
         client.post(
             url="/auth/register",
@@ -20,7 +20,6 @@ class TestAuthLoginSuite:
             },
         )
 
-    @pytest.mark.usefixtures("fixture_user")
     def test_login(self, client: "TestClient") -> None:
         response = client.post(
             url=self._PATH,
@@ -34,28 +33,29 @@ class TestAuthLoginSuite:
         assert response.json()["user"]["username"] == "fixtureUser"
         assert response.json()["user"]["email"] == "fixtureUser@gmail.com"
 
-    @pytest.mark.usefixtures("fixture_user")
     def test_email_error(self, client: "TestClient") -> None:
-        response = client.post(
-            url=self._PATH,
-            json={
-                "password": "1234Abcd!@",
-                "email": "fixtureTest@gmail.com",
-            },
+        self._extracted_from_test_password_error_2(
+            client,
+            "1234Abcd!@",
+            "fixtureTest@gmail.com",
         )
 
-        assert response.status_code == 401
-        assert response.json()["className"] == "EmailOrPasswordError"
-
-    @pytest.mark.usefixtures("fixture_user")
     def test_password_error(self, client: "TestClient") -> None:
-        response = client.post(
-            url=self._PATH,
-            json={
-                "password": "1234Abcd!@Efg",
-                "email": "fixtureUser@gmail.com",
-            },
+        self._extracted_from_test_password_error_2(
+            client,
+            "1234Abcd!@Efg",
+            "fixtureUser@gmail.com",
         )
 
+    def _extracted_from_test_password_error_2(
+        self,
+        client: "TestClient",
+        email: str,
+        password: str,
+    ) -> None:
+        response = client.post(
+            url=self._PATH,
+            json={"password": password, "email": email},
+        )
         assert response.status_code == 401
         assert response.json()["className"] == "EmailOrPasswordError"
