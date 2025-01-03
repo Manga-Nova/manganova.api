224aa59e83981d27ae2ec8f0a9b39e9689c048ee
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-01-03 19:56:52 -0300

    feat: Add project metadata including author, license, project name, and description

--- src/__init__.py
+++ src/__init__.py
@@ -8,3 +8,9 @@
 from src._version import get_version as _get_version
 
 __version__ = _get_version()
+__author__ = "biellSilva"
+__license__ = "MIT"
+__project__ = "MangaNova"
+__description__ = (
+    "MangaNova is an open-source project that provides manga information to users."
+)
